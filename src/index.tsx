6a7f23b4430263cec134daa0f34bf786ba961363
Filip Iulian Pacurar <hello@filipac.net>
2022-06-17 14:19:32 +0300

    Initialize upgrade-to-erdjs10_and_react18

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import './index.css';
+import './assets/sass/theme.scss';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+// import '@elrondnetwork/dapp-core/dist/index.css';
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(<App />);
