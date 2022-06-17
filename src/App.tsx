638ac9a610a61ca7a9293f9c9969d8f98e50e70c
Filip Iulian Pacurar <hello@filipac.net>
2022-06-17 14:19:31 +0300

    Initialize dapp-core-2

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import DappUI from '@elrondnetwork/dapp-core/dist/UI';
+import { DappProvider } from '@elrondnetwork/dapp-core/dist/wrappers';
+
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import Layout from 'components/Layout';
+import PageNotFound from 'pages/PageNotFound';
+import { routeNames } from 'routes';
+import routes from 'routes';
+
+const environment = 'devnet';
+
+const {
+  TransactionsToastList,
+  SignTransactionsModals,
+  NotificationModal,
+  DappCorePages: { UnlockPage }
+} = DappUI;
+
+const App = () => {
+  return (
+    <Router>
+      <DappProvider
+        environment={environment}
+        customNetworkConfig={{ name: 'customConfig', apiTimeout: 6000 }}
+      >
+        <Layout>
+          <TransactionsToastList />
+          <NotificationModal />
+          <SignTransactionsModals className='custom-class-for-modals' />
+          <Routes>
+            <Route
+              path={routeNames.unlock}
+              element={<UnlockPage loginRoute={routeNames.dashboard} />}
+            />
+            {routes.map((route: any, index: number) => (
+              <Route
+                path={route.path}
+                key={'route-key-' + index}
+                element={<route.component />}
+              />
+            ))}
+            <Route path='*' element={<PageNotFound />} />
+          </Routes>
+        </Layout>
+      </DappProvider>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/pages/Dashboard/Transactions/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+  useGetAccountInfo,
+  useGetNetworkConfig,
+  useGetActiveTransactionsStatus
+} from '@elrondnetwork/dapp-core/dist/hooks';
+import DappUI from '@elrondnetwork/dapp-core/dist/UI';
+import { refreshAccount } from '@elrondnetwork/dapp-core/dist/utils';
+import { faExchangeAlt } from '@fortawesome/free-solid-svg-icons';
+import { getTransactions } from 'apiRequests';
+import { contractAddress } from 'config';
+import TransactionsList from './TransactionsList';
+import { StateType } from './types';
+
+const Transactions = () => {
+  const {
+    network: { apiAddress }
+  } = useGetNetworkConfig();
+  const { success, fail, hasActiveTransactions } =
+    useGetActiveTransactionsStatus();
+
+  const [state, setState] = React.useState<StateType>({
+    transactions: [],
+    transactionsFetched: undefined
+  });
+  const account = useGetAccountInfo();
+
+  const fetchData = () => {
+    if (success || fail || !hasActiveTransactions) {
+      getTransactions({
+        apiAddress,
+        address: account.address,
+        timeout: 3000,
+        contractAddress
+      }).then(({ data, success: transactionsFetched }) => {
+        refreshAccount();
+        setState({
+          transactions: data,
+          transactionsFetched
+        });
+      });
+    }
+  };
+
+  React.useEffect(fetchData, [success, fail, hasActiveTransactions]);
+
+  const { transactions } = state;
+
+  return transactions.length > 0 ? (
+    <TransactionsList transactions={transactions} />
+  ) : (
+    <div className='my-5'>
+      <DappUI.PageState
+        icon={faExchangeAlt}
+        className='text-muted fa-3x'
+        title='No Transactions'
+      />
+    </div>
+  );
+};
+
+export default Transactions;
